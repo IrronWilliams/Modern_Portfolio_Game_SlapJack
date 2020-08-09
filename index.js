84b55548f563b940a754edeb68bf0ff57fe1f0a3
Irron Williams <irron.williams@gmail.com>
2020-08-08 17:54:52 -0700

    displaying discard pile on screen

--- index.js
+++ index.js
@@ -75,4 +75,85 @@ function shuffleDeck(deck) {
 }
 
 discardCards = shuffleDeck(discardCards)
-console.log('Shuffled Deck:', discardCards)
+//console.log('Shuffled Deck:', discardCards)
+
+/*dealing cards to players: 
+defining the iterator value and length. defining length saves time by calculate the length of array
+1x vs checking the length of the array each time the loop iterates. when the loop iterates, the iterator will either
+be an odd or even number. if number is even, then push the iterator to player. if iterator is odd, then push the iterator to
+the opponent. using modulus to check even/odd*/
+for (let i=0, l=discardCards.length; i<l; i++) {
+    if(i%2 ===0){
+        playerCards.push(discardCards[i])
+    }else if(i%2 != 0) {
+        opponentCards.push(discardCards[i])
+    }
+}
+
+/*taking a card from either player deck or opponent deck and placing cards in the discard pile. take the top card in deck
+and move to the discardCards. then remove the card from the players deck  */    
+
+discardCards = []
+
+function playerCard(){
+    discardCards.push(playerCards[0])  //pushes the top card, 1st element in array to the discardCards array
+    playerCards.splice(0, 1)  //removes 1 element at index 0. splices out top card from players deck. avoids duplicating cards
+    
+    /*separating string value in discardCards into 2 separate variables in order to apply styles to the values. */ 
+    const currentCard = discardCards[discardCards.length-1] //index of discardCards starts at 0. -1 prevents looking outside of range of array
+    const currentValue = currentCard.substring(0,1) //returns the 1st element of the string in discardCards. 
+    const suit = currentCard.substring(1,2) //returns the 2nd element of the string in discardCards
+    console.log(currentCard, currentValue, suit)
+
+    /*displaying/updating the discardCards deck:
+    the card has 3 elements (card#, artwork & flipped card#). both of the card#'s have the same class, so can easily 
+    reference them in code. Need a loop to iterate thru the multiple card-number elements (right-side-up & flipped). 
+    Change the inner text of the card-number elements to the card just played.
+    
+    Have 4 different options for a suit (H, D, S, C). Using switch/case statement to add the correct suit to card's  
+    inner text. Based upon whether suit is H, D, S, C, using current value of card + a new line and suit symbol. 
+    
+    Additional curly bracket in switch/case statement is because of control scope. it allows me to define 
+    variable within switch case. specifically limiting scope to whats between brackets. w/o the additional {}, 
+    will result in error stating variable has already been declared. 
+    */
+    const cardNumbers = document.getElementsByClassName('card-number')
+    for (let i=0; i<2; i++) {
+         //cardNumbers[i].innerText = currentValue //puts card value on the card. can I use textContent instead of innerText?
+         switch(suit) {
+            case `H`: {
+                cardNumbers[i].innerText = currentValue + "\n♥";
+            }
+            break;
+            case `D`: {
+                cardNumbers[i].innerText = currentValue + "\n♦";
+            }
+            break;
+            case `S`: {
+                cardNumbers[i].innerText = currentValue + "\n♠";
+            }
+            break;
+            case `C`: {
+                cardNumbers[i].innerText = currentValue + "\n♣";
+            }
+            break;
+            default:
+                console.error(`No recognizable suit found`);
+        }
+    }
+    //return currentCard, currentValue, suit
+}
+
+playerCard()
+//console.log('PlayerCards', playerCards)
+//console.log('OpponentCards', opponentCards)
+
+
+
+
+
+
+
+
+
+
